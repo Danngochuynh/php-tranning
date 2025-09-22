b1408de8cb5ab4af5949a255401291496c268458
ngocdan <132124254+Danngochuynh@users.noreply.github.com>
2025-09-22 23:20:46 +0700

    redis

--- logout.php
+++ logout.php
@@ -1,5 +1,21 @@
 <?php
 session_start();
+
+
+$_SESSION = [];
+
+// Xoá cookie PHPSESSID trên trình duyệt
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Huỷ session trong Redis
 session_destroy();
-header('location: login.php');
-?>
+
+// Redirect về trang login
+header('Location: login.php');
+exit;
